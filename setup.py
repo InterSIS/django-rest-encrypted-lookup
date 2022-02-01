e952d95b866cdc1c4174a4590289e887c9cbb4ec
sohanur-shanto <57097733+sohanur-shanto@users.noreply.github.com>
2022-02-01 21:51:16 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
